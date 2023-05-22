dcaa790b0380dc845a5aeb9627d34581b5aaf93d
GustavoGermanos <gustavocg4@al.insper.edu.br>
2023-05-22 10:36:29 -0300

    inicio game_over_screen

--- end_screen.py
+++ end_screen.py
@@ -1,1 +1,30 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+# criar funcao chamada game_over_screen
 
+import pygame
+import random
+from config import WIDTH, HEIGHT, INIT, GAME, QUIT
+from init_screen import init_screen
+from game_screen import game_screen
+from game_over_screen import game_over_screen
+
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Navinha')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(window)
+    elif state == GAME:
+        state = game_screen(window)
+    else:
+        state =  game_over_screen(window)
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
