33ae9eddec003935e764f65944a2090ea4c7bcf8
felipequeirox <fepodqueiroz@gmail.com>
2023-05-24 07:56:42 -0300

    Font e Snd

--- PyGame.py
+++ PyGame.py
@@ -21,28 +21,12 @@ FIGURA_HEIGHT = 38
 
 def load_assets():
     assets = {}
-    assets['background'] = pygame.image.load('assets/Ilha.png').convert()
-    assets['crocodilo_img'] = pygame.image.load('assets/Crocodilo.png').convert_alpha()
+    assets['background'] = pygame.image.load('assets/img/Ilha.png').convert()
+    assets['crocodilo_img'] = pygame.image.load('assets/img/Crocodilo.png').convert_alpha()
     assets['crocodilo_img'] = pygame.transform.scale(assets['crocodilo_img'], (CROCODILO_WIDTH, CROCODILO_HEIGHT))
-    assets['figura_img'] = pygame.image.load('assets/Humano.png').convert_alpha()
+    assets['figura_img'] = pygame.image.load('assets/img/Humano.png').convert_alpha()
     assets['figura_img'] = pygame.transform.scale(assets['figura_img'], (FIGURA_WIDTH, FIGURA_HEIGHT))
-    assets['bullet_img'] = pygame.image.load('assets/laserRed16.png').convert_alpha()
-    explosion_anim = []
-    for i in range(9):
-        # Os arquivos de animação são numerados de 00 a 08
-        filename = 'assets/img/regularExplosion0{}.png'.format(i)
-        img = pygame.image.load(filename).convert()
-        img = pygame.transform.scale(img, (32, 32))
-        explosion_anim.append(img)
-    assets["explosion_anim"] = explosion_anim
-    assets["score_font"] = pygame.font.Font('assets/font/PressStart2P.ttf', 28)
-
-    # Carrega os sons do jogo
-    pygame.mixer.music.load('assets/snd/tgfcoder-FrozenJam-SeamlessLoop.ogg')
-    pygame.mixer.music.set_volume(0.4)
-    assets['boom_sound'] = pygame.mixer.Sound('assets/snd/expl3.wav')
-    assets['destroy_sound'] = pygame.mixer.Sound('assets/snd/expl6.wav')
-    assets['pew_sound'] = pygame.mixer.Sound('assets/snd/pew.wav')
+    assets['bullet_img'] = pygame.image.load('assets/img/laserRed16.png').convert_alpha()
     return assets
 
 # ----- Inicia estruturas de dados
@@ -223,7 +207,6 @@ def game_screen(window):
     lives = 3
 
     # ===== Loop principal =====
-    pygame.mixer.music.play(loops=-1)
     while state != DONE:
         clock.tick(FPS)
 
